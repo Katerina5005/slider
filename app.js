648ad3d094504e048851523652fb289cd78c8961
Katerina5005 <kat51082@gmail.com>
2021-10-29 18:37:31 +0600

    active

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 const upBtn = document.querySelector('.up-button')
 const downBtn = document.querySelector('.down-button')
+const sidebar = document.querySelector('.sidebar')
+const mainSlide = document.querySelector('.main-slide')
 
+const slideCount = mainSlide.querySelectorAll('div').length
+
+let activeSlideIndex = 0
+
+sidebar.style.top = `-${(slideCount - 1) * 100}vh`
+
+upBtn.addEventListener('click', () => {
+    changeSlide('up')
+})
+
+downBtn.addEventListener('click', () => {
+    changeSlide('down')
+})
+
+fubction changeSlide(direction) {
+    if (direction === 'up') {
+        activeSlideIndex++
+            if (activeSlideIndex === slideCount)
+            {
+                activeSlideIndex = 0
+            }
+    } else if (direction === 'down') {
+        activeSlideIndex--
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slideCount - 1
+        }
+    }
+}
